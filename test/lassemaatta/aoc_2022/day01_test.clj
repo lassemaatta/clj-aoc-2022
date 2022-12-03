4d74ddd82fedf4a4972ea237d956725041d5f8a9
Lasse Maatta <lasse.olavi.maatta@gmail.com>
2022-12-03 08:12:50 +0200

    Simplify tests

--- test/lassemaatta/aoc_2022/day01_test.clj
+++ test/lassemaatta/aoc_2022/day01_test.clj
@@ -1,28 +1,11 @@
 (ns lassemaatta.aoc-2022.day01-test
-  (:require [clojure.test :refer [deftest is]]
+  (:require [clojure.test :refer [are deftest]]
             [lassemaatta.aoc-2022.day01 :as day01]
             [lassemaatta.aoc-2022.util :as util]))
 
-(deftest first-small-test
-  (is (= 24000
-         (-> "day01/small-sample.txt"
-             util/read-file
-             day01/first-problem))))
-
-(deftest first-large-test
-  (is (= 70720
-         (-> "day01/large-sample.txt"
-             util/read-file
-             day01/first-problem))))
-
-(deftest second-small-test
-  (is (= 45000
-         (-> "day01/small-sample.txt"
-             util/read-file
-             day01/second-problem))))
-
-(deftest second-large-test
-  (is (= 207148
-         (-> "day01/large-sample.txt"
-             util/read-file
-             day01/second-problem))))
+(deftest day01-test
+  (are [problem filename result] (= result (-> filename util/read-file problem))
+    day01/first-problem  "day01/small-sample.txt" 24000
+    day01/first-problem  "day01/large-sample.txt" 70720
+    day01/second-problem "day01/small-sample.txt" 45000
+    day01/second-problem "day01/large-sample.txt" 207148))
--- test/lassemaatta/aoc_2022/day02_test.clj
+++ test/lassemaatta/aoc_2022/day02_test.clj
@@ -1,28 +1,11 @@
 (ns lassemaatta.aoc-2022.day02-test
-  (:require [clojure.test :refer [deftest is]]
+  (:require [clojure.test :refer [are deftest]]
             [lassemaatta.aoc-2022.day02 :as day02]
             [lassemaatta.aoc-2022.util :as util]))
 
-(deftest first-small-test
-  (is (= 15
-         (-> "day02/small-sample.txt"
-             util/read-file
-             day02/first-problem))))
-
-(deftest first-large-test
-  (is (= 11666
-         (-> "day02/large-sample.txt"
-             util/read-file
-             day02/first-problem))))
-
-(deftest second-small-test
-  (is (= 12
-         (-> "day02/small-sample.txt"
-             util/read-file
-             day02/second-problem))))
-
-(deftest second-large-test
-  (is (= 12767
-         (-> "day02/large-sample.txt"
-             util/read-file
-             day02/second-problem))))
+(deftest day02-test
+  (are [problem filename result] (= result (-> filename util/read-file problem))
+    day02/first-problem  "day02/small-sample.txt" 15
+    day02/first-problem  "day02/large-sample.txt" 11666
+    day02/second-problem "day02/small-sample.txt" 12
+    day02/second-problem "day02/large-sample.txt" 12767))
